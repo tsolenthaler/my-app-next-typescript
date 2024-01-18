962a63de9a69536889161cb77483e07ba736ef87
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-01-18 09:07:45 +0100

    Add button component and login form

--- src/app/dashboard/latest-invoices.tsx
+++ src/app/dashboard/latest-invoices.tsx
@@ -1,5 +1,4 @@
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
